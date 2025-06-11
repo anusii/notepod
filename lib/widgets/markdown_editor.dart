5a7e9d77dbc1a9f531c4938be4a2251c52fd23f1
Graham Williams <graham.williams@togaware.com>
2025-06-11 12:08:25 +1000

    Minor text change.

--- lib/widgets/markdown_editor.dart
+++ lib/widgets/markdown_editor.dart
@@ -57,7 +57,7 @@ Container markdownEditor(
                 maxLines: null,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'Note content',
+                  labelText: 'Note Content',
                 ),
               ),
             ),
