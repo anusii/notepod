ed65b1c9e563333b3391b8b09010cc3649a02d05
Jess Moore <jessclaremoore@gmail.com>
2025-07-13 22:57:37 +1000

    edited attribution

--- lib/utils/save_note.dart
+++ lib/utils/save_note.dart
@@ -21,7 +21,7 @@
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <https://www.gnu.org/licenses/>.
 ///
-/// Authors: Anushka Vidanage, Graham Williams, Jess Moore
+/// Authors: Jess Moore
 
 import 'package:flutter/material.dart';
 
