c63cecf8f73ce87741125d4276323f4c75a47ec3
Jess Moore <jessclaremoore@gmail.com>
2025-07-14 14:06:21 +1000

    removed no underscore in title advice

--- lib/notes/edit_note.dart
+++ lib/notes/edit_note.dart
@@ -108,8 +108,7 @@ class EditNoteState extends State<EditNote>
                       name: noteTitlePred,
                       initialValue: widget.noteData[noteTitlePred],
                       decoration: const InputDecoration(
-                        labelText:
-                            'Note Title (Do not use underscores (_) in title)',
+                        labelText: 'Note Title',
                         labelStyle: TextStyle(
                           color: darkBlue,
                           letterSpacing: 1.5,
