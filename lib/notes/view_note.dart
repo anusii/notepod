bddf24683fa7e1b11e4d006203555319e4167312
Jess Moore <jessclaremoore@gmail.com>
2025-07-15 08:34:20 +1000

    linted import order

--- lib/notes/view_note.dart
+++ lib/notes/view_note.dart
@@ -34,8 +34,8 @@ import 'package:notepod/constants/turtle_structures.dart';
 import 'package:notepod/notes/edit_note.dart';
 import 'package:notepod/notes/list_notes_screen.dart';
 import 'package:notepod/notes/share_note.dart';
-import 'package:notepod/widgets/loading_animation.dart';
 import 'package:notepod/utils/misc.dart';
+import 'package:notepod/widgets/loading_animation.dart';
 
 class ViewNote extends StatefulWidget {
   final Map noteData;
