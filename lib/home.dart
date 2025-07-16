02bf06a213d9ddccbfdcbead57f2675ac6523612
Graham Williams <graham.williams@togaware.com>
2025-07-16 14:43:49 +1000

    Remove title debug printing of each char.

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,12 @@
 /// NotePod - A note taking app with notes shared through private PODs.
 ///
+// Time-stamp: <Wednesday 2025-07-16 14:43:37 +1000 Graham Williams>
+///
 /// Copyright (C) 2023-2025, Software Innovation Institute
 ///
 /// Licensed under the GNU General Public License, Version 3 (the "License");
 ///
 /// License: https://www.gnu.org/licenses/gpl-3.0.en.html
-//
-// Time-stamp: <Wednesday 2025-07-16 09:27:55 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -117,7 +117,6 @@ class HomeState extends State<Home> with SingleTickerProviderStateMixin {
                 key: formKey,
                 onChanged: () {
                   formKey.currentState!.save();
-                  debugPrint(formKey.currentState!.value.toString());
                 },
                 autovalidateMode: AutovalidateMode.disabled,
                 skipDisabled: true,
