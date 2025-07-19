d8ba01517bd0c224274151cc96025143497eafb5
Jess Moore <jessclaremoore@gmail.com>
2025-07-19 21:40:08 +1000

    remove unused import

--- lib/shared_notes/non_readable_note.dart
+++ lib/shared_notes/non_readable_note.dart
@@ -34,7 +34,6 @@ import 'package:notepod/constants/app.dart';
 import 'package:notepod/constants/colours.dart';
 import 'package:notepod/constants/turtle_structures.dart';
 import 'package:notepod/shared_notes/share_external_note.dart';
-import 'package:notepod/shared_notes/shared_note_controls.dart';
 import 'package:notepod/shared_notes/shared_notes_screen.dart';
 import 'package:notepod/widgets/loading_animation.dart';
 import 'package:notepod/widgets/msg_card.dart';
