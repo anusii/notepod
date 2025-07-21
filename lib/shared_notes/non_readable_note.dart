d595d6dc61b622a8d96ff5e8556a707eeabb8265
Jess Moore <jessclaremoore@gmail.com>
2025-07-21 23:06:00 +1000

    linted

--- lib/shared_notes/non_readable_note.dart
+++ lib/shared_notes/non_readable_note.dart
@@ -33,7 +33,6 @@ import 'package:notepod/shared_notes/share_external_note.dart';
 import 'package:notepod/shared_notes/shared_notes_screen.dart';
 import 'package:notepod/widgets/msg_card.dart';
 import 'package:notepod/widgets/note_back_button.dart';
-// import 'package:notepod/widgets/note_del_button.dart';
 import 'package:notepod/widgets/note_share_button.dart';
 
 class NonReadableNote extends StatefulWidget {
