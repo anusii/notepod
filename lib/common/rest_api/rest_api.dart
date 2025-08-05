0179137b10983feb420a491eb8cdb39484048200
Graham Williams <graham.williams@togaware.com>
2025-08-06 08:57:13 +1000

    Remove unnecessary solidpod internal function import

--- lib/common/rest_api/rest_api.dart
+++ lib/common/rest_api/rest_api.dart
@@ -29,7 +29,6 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:solidpod/solidpod.dart';
-import 'package:solidpod/src/solid/api/common_permission.dart';
 
 import 'package:notepod/constants/turtle_structures.dart';
 import 'package:notepod/utils/encryption.dart';
