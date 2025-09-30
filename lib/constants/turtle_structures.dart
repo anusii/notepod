eacba71639d74b9d6422be9f6253f08daef308b6
Jess Moore <jessclaremoore@gmail.com>
2025-10-01 09:45:39 +1000

    use exclude urls containing 'predicates/terms'

--- lib/constants/turtle_structures.dart
+++ lib/constants/turtle_structures.dart
@@ -32,9 +32,8 @@ const mainResDir = 'notepod';
 const noteFileNamePrefix = 'note-';
 
 // IRIs (Internationalized Resource Identifiers). Split the string to avoid
-// lychee link check errors.
-
-String notepodTerms = 'https://solidcommunity.au/' 'predicates/terms#';
+// [20251001 jm] lychee link check excluded (see ci.yaml, flutter.mk)
+String notepodTerms = 'https://solidcommunity.au/predicates/terms#';
 
 String createdDateTimePred = 'createdDateTime';
 String modifiedDateTimePred = 'modifiedDateTime';
