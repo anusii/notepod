1814058d9f1aab2d2d3c91318e1373a255c78311
Jess Moore <jessclaremoore@gmail.com>
2025-10-16 18:11:55 +1100

    fixed typo preventing edit access of shared note

--- lib/shared_notes/view_shared_note.dart
+++ lib/shared_notes/view_shared_note.dart
@@ -156,7 +156,7 @@ class _ViewSharedNoteState extends State<ViewSharedNote> {
                         ),
                       ],
                       // Edit if write access
-                      if (_accessList.contains('control')) ...[
+                      if (_accessList.contains('edit')) ...[
                         NoteActionButton(
                           label: ButtonLabel.edit,
                           icon: const Icon(Icons.edit),
