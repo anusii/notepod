498463cd6ee81e30c6da88a668f033d50a67cb25
Jess Moore <jessclaremoore@gmail.com>
2025-12-08 17:53:14 +1100

    added make sku rule to make product sku for App registration in Apple AppStore

--- support/flutter.mk
+++ support/flutter.mk
@@ -485,10 +485,10 @@ dmg-staging:
 	flutter build macos --release --flavor staging
 
 # Make SKU (stock keeping unit used in app registration on Apple App Store)
-# Created from bundle id
+# Created from app bundle id set in Apple Developer account and used
+# in build
 sku::
-	echo "com.togaware.notepod" | md5
-# 	bash create_sku.sh
+	yq '.targets.Runner.settings.base.PRODUCT_BUNDLE_IDENTIFIER' macos/project.yml | md5
 
 # For the `dev` branch only, update the version sequence number prior
 # to a push (relies on the git.mk being loaded after this
