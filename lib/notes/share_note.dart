5db71295a0161d449502263d4210ad0d85ad6f26
Tony Chen <yang.chen2@anu.edu.au>
2026-02-18 03:22:14 +1100

    Lint

--- lib/notes/share_note.dart
+++ lib/notes/share_note.dart
@@ -27,7 +27,6 @@ library;
 
 import 'package:flutter/material.dart';
 
-import 'package:solidpod/solidpod.dart';
 import 'package:solidui/solidui.dart';
 
 import 'package:notepod/models/own_note.dart';
