dd8b7ef3c252538fa7d1abfb83447a100b72a6ce
Jess Moore <jessclaremoore@gmail.com>
2026-03-13 20:44:49 +1100

    add json methods for Note model

--- lib/models/own_note.dart
+++ lib/models/own_note.dart
@@ -30,8 +30,6 @@ import 'package:solidpod/solidpod.dart';
 import 'package:notepod/constants/turtle_structures.dart';
 import 'package:notepod/models/note.dart';
 
-final String contentPred = 'content';
-
 /// Data model for user's note
 
 class OwnNote {
