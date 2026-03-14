dbdf36f6872555c87d5c8f261f5b1b5e20c5a2e7
Jess Moore <jessclaremoore@gmail.com>
2026-03-14 20:03:08 +1100

    fixed import order

--- lib/shared_notes/list_external_notes.dart
+++ lib/shared_notes/list_external_notes.dart
@@ -29,10 +29,10 @@ import 'package:solidui/solidui.dart';
 import 'package:notepod/constants/app.dart';
 import 'package:notepod/constants/ui.dart';
 import 'package:notepod/models/note.dart';
+import 'package:notepod/notes/non_readable_note.dart';
 import 'package:notepod/notes/share_note.dart';
 import 'package:notepod/notes/view_note.dart';
 import 'package:notepod/shared_notes/list_external_notes_screen.dart';
-import 'package:notepod/notes/non_readable_note.dart';
 import 'package:notepod/utils/get_id.dart';
 import 'package:notepod/widgets/simple_action_button.dart';
 
