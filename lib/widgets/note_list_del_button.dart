e0a68daad7dee4438fd79d8c8452a3aa5a402763
Jess Moore <jessclaremoore@gmail.com>
2026-03-15 13:12:41 +1100

    update deleteFile to comply with solidpod update

--- lib/widgets/note_list_del_button.dart
+++ lib/widgets/note_list_del_button.dart
@@ -29,6 +29,7 @@ import 'package:flutter/material.dart';
 import 'package:solidpod/solidpod.dart';
 import 'package:solidui/solidui.dart';
 
+import 'package:notepod/common/rest_api/file_helper.dart';
 import 'package:notepod/constants/app.dart';
 import 'package:notepod/constants/colours.dart';
 import 'package:notepod/constants/paths.dart';
@@ -89,19 +90,16 @@ class NoteListDelButton extends StatelessWidget {
 
                 // Delete file
                 for (final SelectedNote note in selectedNotes) {
-                  // Create note file path
-                  String noteFilePath = '$basePath/${note.noteFileName}';
-                  debugPrint('Deleting $noteFilePath...');
+                  debugPrint('Deleting ${note.noteUrl}...');
 
                   // Call solid delete file function
-                  if (!isExternal) {
-                    await deleteFile(fileUrl: noteFilePath);
-                  } else {
-                    debugPrint(
-                      '[NoteListDelButton] delete external files not '
-                      'yet supported',
-                    );
-                  }
+                  // Delete file
+                  await NoteFileHelper().deleteNote(
+                    context: context,
+                    filename: note.noteFileName,
+                    isExternal: isExternal,
+                    child: childPage,
+                  );
                 }
 
                 if (context.mounted) {
