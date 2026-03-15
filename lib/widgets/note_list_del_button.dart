d8cc58531875aa419bb2fb676fbe9b0420434db2
Jess Moore <jessclaremoore@gmail.com>
2026-03-15 13:13:15 +1100

    remove unused imports

--- lib/widgets/note_list_del_button.dart
+++ lib/widgets/note_list_del_button.dart
@@ -26,13 +26,11 @@ library;
 
 import 'package:flutter/material.dart';
 
-import 'package:solidpod/solidpod.dart';
 import 'package:solidui/solidui.dart';
 
 import 'package:notepod/common/rest_api/file_helper.dart';
 import 'package:notepod/constants/app.dart';
 import 'package:notepod/constants/colours.dart';
-import 'package:notepod/constants/paths.dart';
 import 'package:notepod/constants/ui.dart';
 import 'package:notepod/models/selected_note.dart';
 import 'package:notepod/widgets/err_card.dart';
