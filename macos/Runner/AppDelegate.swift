8513fdeeef5b2222419f552d96f09309673e58f7
Amoghhosamane <amoghvarsh9614@gmail.com>
2026-05-26 22:17:15 +0530

    Fix macOS foreground issue: activate app on launch to remove 'Failed to foreground app; open returned 1' warning

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,8 +1,11 @@
 import Cocoa
 import FlutterMacOS
 
-@main
-class AppDelegate: FlutterAppDelegate {
+    override func applicationDidFinishLaunching(_ notification: Notification) {
+        super.applicationDidFinishLaunching(notification)
+        NSApp.activate(ignoringOtherApps: true)
+    }
+
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
