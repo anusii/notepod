78b34297a2caa230afc80f7a93880e5e75385091
Graham Williams <graham.williams@togaware.com>
2026-06-05 13:37:16 +1000

    Updated oidc config.

--- lib/notepod.dart
+++ lib/notepod.dart
@@ -1,6 +1,6 @@
 /// NotePod - The primary [MaterialApp] widget.
 ///
-// Time-stamp: <Friday 2025-10-24 11:59:49 +1100 Graham Williams>
+// Time-stamp: <Friday 2026-06-05 13:21:47 +1000 Graham Williams>
 ///
 /// Copyright (C) 2025, Software Innovation Institute, ANU.
 ///
@@ -72,14 +72,8 @@ class NotePod extends StatelessWidget {
         redirectUris: const [
           'https://solidcommunity.au/apps/notepod/redirect.html',
           'http://localhost:4400/redirect',
-          'com.example.notepod://redirect',
+          'com.togaware.notepod://redirect',
         ],
-        postLogoutRedirectUris: const [
-          'https://solidcommunity.au/apps/notepod/redirect.html',
-          'http://localhost:4400/redirect',
-          'com.example.notepod://redirect',
-        ],
-        autoLogin: true,
         child: AppHomePage(
           childPage: ListMyNotesScreen(
             scaffoldController: scaffoldController,
