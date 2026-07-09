fc0fd44e45e472618831777612c5f02b4f219c65
Tony Chen <yang.chen2@anu.edu.au>
2026-07-10 03:49:25 +1000

    Move client-profile.jsonld to root directory

--- lib/notepod.dart
+++ lib/notepod.dart
@@ -25,6 +25,7 @@
 
 library;
 
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 
 import 'package:solidui/solidui.dart';
@@ -66,13 +67,13 @@ class NotePod extends StatelessWidget {
               'to access the markdown note files\n'
               'stored in your POD.',
         ),
-        clientId:
-            'https://notepod.solidcommunity.au/client-profile.jsonld',
-        redirectUris: const [
-          'https://notepod.solidcommunity.au/redirect.html',
-          'http://localhost:4400/redirect.html',
-          'com.togaware.notepod://redirect',
-        ],
+        clientId: 'https://dev.empwr.au/notepod/client-profile.jsonld',
+        redirectUris: kIsWeb
+            ? ['${Uri.base.origin}/redirect.html']
+            : const [
+                'com.togaware.notepod://redirect',
+                'http://localhost:4400/redirect.html',
+              ],
         child: AppHomePage(
           childPage: ListMyNotesScreen(
             scaffoldController: scaffoldController,
